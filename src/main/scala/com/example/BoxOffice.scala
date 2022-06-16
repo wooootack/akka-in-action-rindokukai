9a5abef12e09190b550ea98b5a7a106004d6429a
Kyosuke Awata <kyou0209@gmail.com>
2022-06-16 20:37:58 +0900

    wip

--- src/main/scala/com/example/BoxOffice.scala
+++ src/main/scala/com/example/BoxOffice.scala
@@ -1,13 +1,26 @@
 package com.example
 
+import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext}
 
 
 object BoxOffice {
   sealed trait Command
+
+  case class CreateEvent(name: String, tickets: Int) extends Command
 }
 
 class BoxOffice(context: ActorContext[BoxOffice.Command])
   extends AbstractBehavior[BoxOffice.Command](context) {
+
   import BoxOffice._
+
+  def createTicketSeller(name: String) =
+    context.spawn()
+
+  override def onMessage(msg: BoxOffice.Command): Behavior[BoxOffice.Command] =
+    msg match {
+      case CreateEvent(name, tickets) =>
+
+    }
 }
