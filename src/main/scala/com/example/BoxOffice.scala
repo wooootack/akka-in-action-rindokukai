4218c9c9b7b935963a9e62d633b6de4447598044
Kyosuke Awata <kyou0209@gmail.com>
2022-06-30 20:27:29 +0900

    wip

--- src/main/scala/com/example/BoxOffice.scala
+++ src/main/scala/com/example/BoxOffice.scala
@@ -7,7 +7,11 @@ import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext}
 object BoxOffice {
   sealed trait Command
 
-  case class CreateEvent(name: String, tickets: Int) extends Command
+  case class CreateEvent(name: String, tickets: Int, replyTo) extends Command
+
+  sealed trait EventResponse
+  case class EventCreated(event: Event) extends EventResponse
+  case object EventExists extends EventResponse
 }
 
 class BoxOffice(context: ActorContext[BoxOffice.Command])
@@ -20,12 +24,13 @@ class BoxOffice(context: ActorContext[BoxOffice.Command])
 
   override def onMessage(msg: BoxOffice.Command): Behavior[BoxOffice.Command] =
     msg match {
-      case CreateEvent(name, tickets) =>
+      case CreateEvent(name, tickets, replyTo) =>
         def create() = {
           val eventTickets = createTicketSeller(name)
           val newTickets = (1 to tickets).map { ticketId =>
             TicketSeller.Ticket(ticketId)
           }.toVector
         }
+        context.child(name).fold(create()).(_ => replyTo ! )
     }
 }
